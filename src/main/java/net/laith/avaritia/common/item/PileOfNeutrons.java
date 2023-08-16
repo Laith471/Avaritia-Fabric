9efccdbd5e084098dab6422c80ba3f6a1d2881ba
Laith471 <liathaqeel@gmail.com>
2023-08-16 11:50:43 +0300

    Add info for the neutron

--- src/main/java/net/laith/avaritia/common/item/PileOfNeutrons.java
+++ src/main/java/net/laith/avaritia/common/item/PileOfNeutrons.java
@@ -1,5 +1,6 @@
 package net.laith.avaritia.common.item;
 
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -17,6 +18,11 @@ public class PileOfNeutrons extends Item {
     @Override
     public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
         super.appendTooltip(stack, world, tooltip, context);
+        if(Screen.hasShiftDown()) {
+            tooltip.add(Text.translatable("item.avaritia.pile_of_neutrons_source.tooltip"));
+        } else {
+            tooltip.add(Text.translatable("item.avaritia.pile_of_neutrons_see_source.tooltip"));
+        }
         tooltip.add(Text.translatable("item.avaritia.pile_of_neutrons.tooltip"));
     }
 }
