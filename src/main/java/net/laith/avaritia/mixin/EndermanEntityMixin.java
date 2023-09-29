00a357fc308912f6b443dd7c1223b73f68a5e996
Laith471 <liathaqeel@gmail.com>
2023-09-29 18:36:21 +0300

    -code cleanup

--- src/main/java/net/laith/avaritia/mixin/EndermanEntityMixin.java
+++ src/main/java/net/laith/avaritia/mixin/EndermanEntityMixin.java
@@ -3,13 +3,10 @@ package net.laith.avaritia.mixin;
 import net.laith.avaritia.util.helpers.BooleanHelper;
 import net.minecraft.entity.mob.EndermanEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(EndermanEntity.class)
 public class EndermanEntityMixin {
